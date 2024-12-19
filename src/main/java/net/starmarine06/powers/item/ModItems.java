be6e1405dfa5a87bc002ccda0d79195e8b22de25
Starmarine06 <devnambiar06@gmail.com>
2024-12-19 09:48:08 +0530

    Initialz

--- src/main/java/net/starmarine06/powers/item/ModItems.java
+++ src/main/java/net/starmarine06/powers/item/ModItems.java
@@ -5,18 +5,24 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.starmarine06.powers.Powers;
 
 public class ModItems {
-    public static final Item FLY = registerItem("fly",new Item(new Item.Settings()));
+    public static final Item CLOUD = register("cloud_item",new Item.Settings());
 
-    private static Item registerItem(String name, Item item){
-        return Registry.register(Registries.ITEM, Identifier.of(Powers.MOD_ID, name), item);
+    private static Item register(String name, Item.Settings itemSettings) {
+        Identifier id = Identifier.of(Powers.MOD_ID, name);
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+        Item.Settings settings = itemSettings.registryKey(key);
+
+        return Registry.register(Registries.ITEM, key, new Item(settings));
     }
     public static void registerModItems(){
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(FLY);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries ->{
+            entries.add(CLOUD);
         });
     }
 }
